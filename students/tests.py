a335fd92a28a5c12d0950dd3b1662f17f877f332
Alexandr <godetskiy@ya.ru>
2012-02-02 12:24:21 +0200

    Test created

--- students/tests.py
+++ students/tests.py
@@ -4,13 +4,37 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
+#-*- coding:utf-8 -*-
 from django.test import TestCase
-
+from django.test.client import Client
+from django.contrib.auth.models import User
+from students.models import Group
+from datetime import date
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    username = 'user'
+    password = 'user'
+    email = 'user@example.com'
+
+    def setUp(self):
+        User.objects.create_superuser(username=self.username, email=self.email, password=self.password)
+
+    def test_basic_students(self):
+        c = Client()
+
+        actual = c.login(username=self.username, password=self.password)
+        self.assertEqual(actual, True)
+
+        test_title = 'Test group'
+        actual = c.post('http://testserver/add/', {'title': test_title,}).status_code
+        self.assertEqual(actual, 302)
+
+        test_group = Group.objects.get(title=test_title).id
+
+        link = 'http://testserver/' + str(test_group) + '/add/'
+        actual = c.post(link, {'fio': 'Test student',
+                               'birth_date': date.today(),
+                               'number': '10005',
+                               'student_group': test_group,})
+        self.assertEqual(actual.status_code, 302)
+
